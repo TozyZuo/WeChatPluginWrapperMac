72b0c8b3fce5bf2489b9862764f2cc1e7f6c23ca
TozyZuo <haoyuan.lhy@alibaba-inc.com>
2018-09-14 17:30:39 +0800

    update sh

--- Other/Install.sh
+++ Other/Install.sh
@@ -24,7 +24,7 @@ fi
 # 对 WeChat 赋予权限
 if [ ! -w "$wechat_path" ]
 then
-echo -e "\n\n为了将小助手写入微信, 请输入密码 ： "
+echo -e "\n\n为了将插件写入微信, 请输入密码 ： "
 sudo chown -R $(whoami) "$wechat_path"
 fi
 
@@ -44,4 +44,4 @@ ${shell_path}/insert_dylib --all-yes "${framework_path}/${framework_name}" "$app
 framework_name="WeChatPlugin_TKkk"
 framework_path="${app_bundle_path}/${framework_name}.framework"
 cp -r "${shell_path}/Products/Debug/${framework_name}.framework" ${app_bundle_path}
-${shell_path}/insert_dylib --all-yes "${framework_path}/${framework_name}" "$app_executable_backup_path" "$app_executable_path"
+${shell_path}/insert_dylib --all-yes "${framework_path}/${framework_name}" "$app_executable_path" "$app_executable_path"
